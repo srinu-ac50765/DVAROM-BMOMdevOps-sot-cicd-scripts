6568c3ddcb88184b00c7263d7a5daec9e718c876
Ning <ning.li@centurylink.com>
2020-05-06 16:36:04 -0600

    Change project for demo purpose

--- checkProjectDeploy.py
+++ checkProjectDeploy.py
@@ -11,6 +11,6 @@ logging.basicConfig()
 logging.root.setLevel(logging.INFO)
 log=logging.getLogger("checkProjectDeploy")
 
-#bmGocdCommon.checkProjectDeploy("BMP_DirectoryListingBusinessService", "TEST1", "release/january20")        
-bmGocdCommon.checkProjectDeploy("account-management-business-service", "TEST1", "release/march20")
+bmGocdCommon.checkProjectDeploy("BMP_DirectoryListingBusinessService", "TEST1", "release/january20")        
+#bmGocdCommon.checkProjectDeploy("account-management-business-service", "TEST1", "release/march20")
 #bmGocdCommon.checkProjectDeploy("BMP_AccountManagmentService", "TEST1", "release/january20")
